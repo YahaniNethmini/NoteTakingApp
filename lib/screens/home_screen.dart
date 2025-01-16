4881970476fe77890d19a9b628d74c5579a2d6dc
Yahani Nethmini <tynperera@gmail.com>
2025-01-16 22:19:42 +0530

    update home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/services/database_helper.dart';
+
+import '../model/notes_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +11,144 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final DatabaseHelper _databaseHelper = DatabaseHelper();
+  List<Note> _notes = [];
+  final List<Color> _noteColors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.yellow,
+    Colors.purple,
+    Colors.orange,
+    Colors.pink,
+    Colors.teal,
+    Colors.indigo,
+    Colors.cyan,
+  ];
+
+  @override
+  void initState() {
+    //TODO: implement initState
+    super.initState();
+    _loadNotes();
+  }
+
+  Future<void> _loadNotes() async {
+    final notes = await _databaseHelper.getNotes();
+    setState(() {
+      _notes = notes;
+    });
+  }
+  
+  String _formatDateTime(String dateTime) {
+    final DateTime dt = DateTime.parse(dateTime);
+    final now = DateTime.now();
+
+    if (dt.year == now.year && dt.month == now.month && dt.day == now.day) {
+      return "Today, ${dt.hour.toString().padLeft(2, '0')}:${dt.minute
+          .toString().padLeft(2, '0')}";
+    }
+    return "${dt.day}/${dt.month}/${dt.year}, ${dt.hour.toString().padLeft(2, '0')}:${dt.minute.toString()
+        .padLeft(2, '0')}";
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+            "My Notes",
+        ),
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(16),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+          ),
+          itemCount: _notes.length,
+          itemBuilder: (context, index){
+            final note = _notes[index];
+            final color = Color(int.parse(note.color));
+
+            return GestureDetector(
+              onTap: () async {
+                // await Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //         builder: (context) => ViewNoteScreen(note: note)
+                //     )
+                // );
+                _loadNotes();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 4,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      note.title,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      note.content,
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 16,
+                      ),
+                      maxLines: 4,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Spacer(),
+                    Text(
+                        _formatDateTime(note.dateTime),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            await Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => AddEditNoteScreen(),
+                )
+            );
+            _loadNotes();
+          },
+        child: Icon(Icons.add),
+        backgroundColor: Color(0XFF50C878),
+        foregroundColor: Colors.white,
+      ),
+    );
   }
 }
